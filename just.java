3e3ab45c33c43ac344c7b4135e78837c3bf7d272
ayush-saint <ayush22kumar37@gamil.com>
2024-11-04 11:05:33 +0530

    origin

--- just.java
+++ just.java
@@ -1,5 +1,11 @@
-public class just{
-    public static void main(String[] args){
-        System.out.println("hello not ");
+import java.util.Scanner;
+
+public class just {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter the name ");
+        String name = scanner.next();
+        System.out.println(name);
+        scanner.close();
     }
 }
