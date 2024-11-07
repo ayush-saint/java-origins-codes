a965215b6a1526c14b3e0f4e0a78ca7de6f633bc
ayush-saint <ayush22kumar37@gamil.com>
2024-11-07 11:31:19 +0530

    for loop example

---
+++ ForLoop.java
@@ -0,0 +1,9 @@
+//program_print a to z using for loop 
+
+public class ForLoop{
+    public static void main(String[] args){
+        for(char a = 'a'; a<='z' ; a++){
+            System.out.println(a);
+        }
+    }
+}
