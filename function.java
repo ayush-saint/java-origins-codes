dbcafcb9cd5fa02db758745732cfd0f66dfa015a
ayush-saint <ayush22kumar37@gamil.com>
2024-11-11 15:23:35 +0530

    sum using function and returning int

--- function.java
+++ function.java
@@ -1,26 +1,18 @@
 import java.util.Scanner;
 public class function{
     public static void main(String[] args){
-        sum();
-        sum();
-        sum();
-        sum();
-        sum();
-        sum();
-        sum();
-        sum();
-        sum();
-        sum();
-
+        int ans = sum();
+        System.out.println(ans);
+    
     }
-        static void sum(){
+        static int sum(){
             Scanner scanner = new Scanner(System.in);
             System.out.println("enter the first number ");
             int a = scanner.nextInt();
             System.out.println("enter the second number ");
             int b = scanner.nextInt();
             int sum = a+b;
-            System.out.println(sum);
+            return sum;
         }
     } 
     
