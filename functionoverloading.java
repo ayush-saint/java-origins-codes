e238f0fdf323b0672266ec7d82342c78c53cd70b
Ayush Kumar <ayush22kumar37@gmail.com>
2024-11-11 18:29:28 +0530

    function overloading with samw primitive data type

--- functionoverloading.java
+++ functionoverloading.java
@@ -1,7 +1,14 @@
 public class functionoverloading{
     public static void main(String[] args){
-        fun(88);
+        int ans =fun(88,33);
+        System.out.println(ans);
 
+    }
+    static int fun(int a, int b){
+        return a*b;
+    }
+    static int fun(int a, int b,int c){
+        return a+b+c;
     }
     static void fun(int a){
         System.out.println(a);
