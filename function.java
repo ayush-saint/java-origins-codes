4dae1e18f24953de9c4962d6134b2d96addf122a
ayush-saint <ayush22kumar37@gamil.com>
2024-11-11 15:34:08 +0530

    program for sum of two number where numbers are passed in function call

--- function.java
+++ function.java
@@ -1,18 +1,15 @@
+//program for sum of two number where numbers are passed in function call
+
 import java.util.Scanner;
 public class function{
     public static void main(String[] args){
-        int ans = sum();
+        int ans = sum(40,44);
         System.out.println(ans);
     
     }
-        static int sum(){
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("enter the first number ");
-            int a = scanner.nextInt();
-            System.out.println("enter the second number ");
-            int b = scanner.nextInt();
+      static int sum(int a,int b){
             int sum = a+b;
             return sum;
-        }
+      }
     } 
     
