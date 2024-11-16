7645032411ce05cbfa993659bb83ddda1042d727
ayush-saint <ayush22kumar37@gamil.com>
2024-11-16 19:24:28 +0530

    code with array in java where input is a String

---
+++ Arrays3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Arrays3{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter some String : ");
+        String[] str = new String[5];
+        for(int i =0 ;i<str.length ;i++){
+            str[i]=scanner.next();
+        }
+        System.out.println(Arrays.toString(str));
+    }
+}
