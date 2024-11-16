fe30ee610cc10e9eac4a26a8472e1fa74ad3751b
ayush-saint <ayush22kumar37@gamil.com>
2024-11-16 18:45:15 +0530

    code with array in java where input is done by enhanced for loop

---
+++ Arrays2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Arrays2{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the 5 number ");
+        int[] arr = new int[5];
+
+        for(int i = 0 ;i<arr.length;i++){
+            arr[i]=scanner.nextInt();
+        }
+    
+        for (int num : arr){
+            System.out.println(num + " ");
+        }
+
+    }
+}
+
