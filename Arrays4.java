71f8ab13676e60fbb4181cc4d8cf32e7b8fb9a39
ayush-saint <ayush22kumar37@gamil.com>
2024-11-29 14:53:43 +0530

    code to print 5 rollno with array syntax two

--- Arrays4.java
+++ Arrays4.java
@@ -1,8 +1,16 @@
-//write a program in java to store 5 rollno of class using syntax of Array type 1 
+//write a program in java to store 5 rollno of class using syntax of Array type 2
 import java.util.Scanner;
 import java.util.Arrays;
 public class Arrays4{
     public static void main(String[] args){
+        int[] rollno = {1,33,55,53,55};
+        for(int i=0;i<rollno.length ;i++){
+            System.out.println(rollno[i]);
+        }
+        }
+    }
+
+/*  public static void main(String[] args){
         int[] rollno = new int[5];
         rollno[0]=1;
         rollno[1]=2;
@@ -11,6 +19,6 @@ public class Arrays4{
         rollno[4]=5;
         for(int i=0;i<rollno.length;i++){
             System.out.println(rollno[i]);
-        }
-    }
-}
+        }   */
+
+    
