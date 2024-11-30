e49458694ed18b5f9ac4b88ec27389c4ef52cc03
ayush-saint <ayush22kumar37@gamil.com>
2024-11-30 19:34:57 +0530

    /program to print the entered 2d array with scanner

---
+++ multidarray.java
@@ -0,0 +1,33 @@
+//program to print the entered 2d array with scanner 
+import java.util.Scanner;
+import java.util.Arrays;
+public class multidarray{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enter the no of row ");
+        int row = scanner.nextInt();
+
+        System.out.println("enter the no of column ");
+        int column = scanner.nextInt();
+
+        int[][] arrr = new int[row][column];
+        
+        System.out.println("enter the numbers of array ");
+
+        for(int i = 0 ; i<row ; i++){
+            for(int j =0 ;j<column ; j++){
+                arrr[i][j]= scanner.nextInt();
+            }
+        }
+
+        System.out.println("numbers of 2d array are ");
+        
+        for(int i=0; i<row; i++){
+            for(int j=0; j<column ; j++){
+                System.out.println(arrr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
