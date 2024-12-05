2cea3099f3105a0bdd793d7cde84613270ee72c5
ayush-saint <ayush22kumar37@gamil.com>
2024-12-05 21:09:53 +0530

    codes with scanne in java

---
+++ scanner.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class scanner{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter some string ");
+        String name = sc.next();
+        System.out.println(name);
+    }
+}
