f7333359f110b1e6288fa1395799f5b98c799ec0
Ayush Kumar <ayush22kumar37@gmail.com>
2024-12-06 12:56:22 +0530

    if else conditional statements diff questions

--- ifelseconditionalstatement.java
+++ ifelseconditionalstatement.java
@@ -1,4 +1,7 @@
 //program using if else conditional statement 
+
+/*__code to decide whether the person is adult or minor
+
 import java.util.*;
 class ifelseconditionalstatement{
     public static void main(String[] args){
@@ -15,4 +18,43 @@ class ifelseconditionalstatement{
 
     }
 
+}*/
+
+/*__code to print that the given no is odd or even 
+
+import java.util.*;
+class ifelseconditionalstatement{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter the number to cheak whether it's odd or even : ");
+        int a = scanner.nextInt();
+        if(a % 2 == 0){
+            System.out.println("even ");
+        }else{
+            System.out.println("odd");
+        };
+        scanner.close();
+    }
+}*/
+
+/*__input two number a and b , print equal if a=b,print greater if a>b, print lesser if a<b  */
+import java.util.*;
+class ifelseconditionalstatement{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter the number a : ");
+        int a = scanner.nextInt();
+        System.out.print("enter the number b : ");
+        int b = scanner.nextInt();
+        if(a>b){
+            System.out.println("greater");
+        }
+        if(a<b){
+            System.out.println("lesser");
+        }else{
+            System.out.println("equal");
+        };
+        scanner.close();
+
+    }
 }
