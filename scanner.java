74999c1dd878c20c10332136695e78f2492dab14
Ayush Kumar <ayush22kumar37@gmail.com>
2024-12-06 12:29:40 +0530

    code for if else conditional statements

--- scanner.java
+++ scanner.java
@@ -1,12 +1,13 @@
+//program to add two number to understand scanners
 import java.util.*;
 class scanner{
     public static void main(String[] args){
-        Scanner a = new Scanner(System.in);
-        System.out.println("enter a :");
-        int aa = a.nextInt();
-        System.out.println("enter b :");
-        int bb = a.nextInt();
-        int sum = aa+bb;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the first a : ");
+        int a = sc.nextInt();
+        System.out.println("enter the number b : ");
+        int b = sc.nextInt();
+        int sum = a+b;
         System.out.println(sum);
     }
 }
