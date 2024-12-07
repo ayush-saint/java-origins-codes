8f7daf49e62fee72472cc6a122f51ad62a3f77c5
Ayush Kumar <ayush22kumar37@gmail.com>
2024-12-07 13:40:32 +0530

    Q 1 of For Loop

---
+++ Q1ForLoop.java
@@ -0,0 +1,9 @@
+// code to printing counting from 0 to 10 
+
+public class Q1ForLoop{
+    public static void main(String[] args){
+        for(int i = 0 ; i<11 ; i++){
+            System.out.println(i);
+        }
+    }
+}
