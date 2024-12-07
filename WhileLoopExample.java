6a0f37072bd2323728c0f57dcc824ab387a3b18d
ayush-saint <ayush22kumar37@gamil.com>
2024-12-07 13:47:18 +0530

    While Loop Example

---
+++ WhileLoopExample.java
@@ -0,0 +1,11 @@
+// program to print 0 to 10 by while loop 
+
+public class WhileLoopExample{
+    public static void main(String[] args){
+        int i = 0 ;
+        while(i<11){
+            System.out.println(i);
+            i++;
+        };
+    }
+}
