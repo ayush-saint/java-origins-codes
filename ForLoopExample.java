039b685c6e7bbc0c6566a712266b150e90e08700
Ayush Kumar <ayush22kumar37@gmail.com>
2024-12-07 13:25:52 +0530

    For Loop Example

---
+++ ForLoopExample.java
@@ -0,0 +1,9 @@
+// program to print hello world 10 times 
+
+public class ForLoopExample{
+    public static void main(String[] args){
+        for(int a = 0 ; a<10 ; a=a+1){
+            System.out.println("hello world ");
+        };
+    }
+}
