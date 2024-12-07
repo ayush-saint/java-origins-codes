170d7b4793633e76b80cb3855e4d9592dce7b0c3
ayush-saint <ayush22kumar37@gamil.com>
2024-12-07 14:11:34 +0530

    Do While Loop Example

---
+++ DoWhileLoopExample.java
@@ -0,0 +1,11 @@
+// program to print 0 to 10 using do while loop 
+
+public class DoWhileLoopExample{
+    public static void main(String[] args){
+        int i = 0;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<11);
+    }
+}
