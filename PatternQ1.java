e3dd92ceaafb2cd66205b5c4df4fc13f1ddcc9d6
ayush-saint <ayush22kumar37@gamil.com>
2024-12-09 13:55:24 +0530

    Question no 1 of Pattern Problem

---
+++ PatternQ1.java
@@ -0,0 +1,18 @@
+/*print pattern 
+  *****
+  *****
+  *****
+  *****
+ */
+//for this question we will going to use for loop 
+
+public class PatternQ1{
+    public static void main(String[] args){
+        for(int i = 0; i<4 ; i++){
+            for(int j = 0 ; j<5 ; j++){
+                System.out.print("*");
+            }
+            System.out.println("*");
+        };
+    }
+}
