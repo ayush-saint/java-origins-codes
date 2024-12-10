ff49fbc72fbb22de5c7da04b26bc15320c204129
ayush-saint <ayush22kumar37@gamil.com>
2024-12-10 19:40:15 +0530

    Question no 11 of Pattern Problem

---
+++ PatternQ11.java
@@ -0,0 +1,23 @@
+/*    Print Pattern 
+    *****
+   *****
+  *****
+ *****
+*****      (Solid Rhombus)  */
+
+public class PatternQ11{
+    public static void main(String[] args){
+        int n =5 ;
+        for(int i = n; i>=1 ; i--){
+            for(int j =1 ; j<i ; j++){
+                System.out.print(" ");
+            };
+            for(int j=1 ; j<=n ; j++){
+                System.out.print("*");
+            };
+            System.out.println();
+        }
+
+    }
+
+}
