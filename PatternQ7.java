74c9b4f68902b8f162a2e6c8e0cac6ae2631d4f3
Ayush Kumar <ayush22kumar37@gmail.com>
2024-12-10 11:44:00 +0530

    Question no 7 of Pattern Problem

--- PatternQ7.java
+++ PatternQ7.java
@@ -7,8 +7,8 @@
 
 public class PatternQ7{
     public static void main(String[] args){
-        for(int i = 5 ; i>=1 ; i--){
-            for(int j = 1 ; j<=i ; j++){
+        for(int i =1 ; i<=5 ; i++){
+            for(int j =1 ; j<=5-i+1 ; j++){
                 System.out.print(j);
             }
             System.out.println();
