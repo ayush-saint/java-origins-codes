52fe65798773a327af641bab9e8223bb608f07cc
ayush-saint <ayush22kumar37@gamil.com>
2024-12-12 20:38:05 +0530

    Java Question 6

---
+++ JavaQ6.java
@@ -0,0 +1,8 @@
+// Write the infinity loop using do while condition 
+public class JavaQ6{
+    public static void main(String[] args){
+        do{
+            System.out.println("this is infinite");
+        }while(true);
+    }
+}
