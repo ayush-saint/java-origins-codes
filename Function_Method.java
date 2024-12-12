ab90c9b37a483a44bb23e338f12ce2f1f709b78b
ayush-saint <ayush22kumar37@gamil.com>
2024-12-12 13:05:13 +0530

    Function(Method) in java

---
+++ Function_Method.java
@@ -0,0 +1,17 @@
+// In java function is called as Method 
+
+import java.util.Scanner;
+
+public class Function_Method{
+    public static void PrintMyName(String name){
+        System.out.println(name);
+        return;
+    }
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter your name ");
+        String name = scanner.nextLine();
+        PrintMyName(name);
+    }
+
+}
