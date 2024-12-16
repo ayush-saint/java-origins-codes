d2644a0f472b90cd9b7aae9ee7ee6f23b66b1350
Ayush Kumar <ayush22kumar37@gmail.com>
2024-12-16 12:05:05 +0530

    Arrays intro code with diff syntax

---
+++ Arrays_intro2.java
@@ -0,0 +1,9 @@
+//Storing array in different way
+import java.util.Arrays;
+public class Arrays_intro2{
+public static void main(String[] args){
+    int[] number = {1,2,4,5,4,440};
+
+    System.out.println(Arrays.toString(number));
+}
+}
