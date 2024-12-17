34b03be6dc26dd9f44cc7edc1b76bc0abf9fd1da
Ayush Kumar <ayush22kumar37@gmail.com>
2024-12-17 12:17:43 +0530

    Strig intro code

---
+++ Strings_intro.java
@@ -0,0 +1,7 @@
+//Print a string 
+public class Strings_intro{
+    public static void main(String[] args){
+        String name  = "my name is ayush kumar ";
+        System.out.println(name);
+    }
+}
