ec53523d4c3ace970e549776a5ff50851a80ab65
Ayush Kumar <ayush22kumar37@gmail.com>
2024-12-21 13:02:30 +0530

    git bit masking

---
+++ test.java
@@ -0,0 +1,11 @@
+public class test{
+    public static void main(String[] args){
+        fun(88);
+    }
+    static void fun(int a){
+        System.out.println(a);
+    }
+    static void fun(String b){
+        System.out.println(b);
+    }
+}
