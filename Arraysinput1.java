73470b953183e9900a221de53678aee951b0ebb7
ayush-saint <ayush22kumar37@gamil.com>
2024-12-31 15:10:58 +0530

    Arrays input 1

---
+++ Arraysinput1.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class Arraysinput1{
+    public static void main(String[] args){
+        int[] arr ={33,22,33,44,3,2,1,3};
+        for(int i = 0 ; i<arr.length ; i++){
+            System.out.print(arr[i]+" ");
+        }
+        }
+    }
