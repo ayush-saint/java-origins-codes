8cd5c0578700704249972ba7d4b0b4485e6814d2
Ayush Kumar <ayush22kumar37@gmail.com>
2024-12-31 15:42:24 +0530

    Arrays passing in function

---
+++ ArraysinMethod.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class ArraysinMethod{
+    public static void main(String[] args){
+        int[] num = {3,44,6,33,2,22,33,4};
+        array(num);
+    }
+    public static void array(int[] arr){
+        System.out.print(Arrays.toString(arr));
+    }
+}
+
