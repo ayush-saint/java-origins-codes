fd6a4ea0c12aa27fe968b12f59410c5472a890d5
Ayush Kumar <ayush22kumar37@gmail.com>
2024-12-31 14:27:51 +0530

    Arrays Example 1

---
+++ Arraysintro.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+public class Arraysintro{
+    public static void main(String[] args){
+        int[] arr = {1,2,3,44,33,22,1};
+        System.out.println(Arrays.toString(arr));
+    }
+}
