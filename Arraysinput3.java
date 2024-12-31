8c3a8a6c1717209610243684149eb110d60d6b44
Ayush Kumar <ayush22kumar37@gmail.com>
2024-12-31 15:22:17 +0530

    Arrays input 3

---
+++ Arraysinput3.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+public class Arraysinput3{
+    public static void main(String[] args){
+        int[] arr = {3,3,22,44,66,22,3};
+        System.out.print(Arrays.toString(arr));
+    }
+}
