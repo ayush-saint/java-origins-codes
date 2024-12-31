54c4db651525ca7f36e25fb7661001d95f30d4bb
ayush-saint <ayush22kumar37@gamil.com>
2024-12-31 14:30:07 +0530

    Arrays Example 2

---
+++ Arraysintro2.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+public class Arraysintro2{
+    public static void main(String[] args){
+        int[] arr = {2,33,44,5,66};
+        System.out.println(Arrays.toString(arr));
+    }
+}
