5024bd972deb2c933e8a02a44d8f00e27b8eb854
ayush-saint <ayush22kumar37@gamil.com>
2024-12-31 15:22:17 +0530

    Arrays input 3

--- Arraysinput2.java
+++ Arraysinput2.java
@@ -3,7 +3,7 @@ public class Arraysinput2{
     public static void main(String[] args){
         int[] arr = {2,3,4,22,33,55,66};
         for(int num : arr){
-            System.out.print(num);
+            System.out.print(num+" ");
         }
     }
 }
