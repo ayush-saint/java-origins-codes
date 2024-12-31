099d584be30558ffdbae010a457697efef8e1e3e
Ayush Kumar <ayush22kumar37@gmail.com>
2024-12-31 14:36:35 +0530

    Arrays Example 3

---
+++ Arraysintro3.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class Arraysintro3{
+    public static void main(String[] args){
+        int[] arr = {3,3,34,44,22};
+        System.out.println(arr[1]);
+        //in java index are counted from (left to right) here from (3 to 22)
+
+    }
+}
