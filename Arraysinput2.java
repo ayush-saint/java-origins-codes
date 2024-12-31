f95b59baf12e60e1a2a21832aa90a6ffa343f3bb
Ayush Kumar <ayush22kumar37@gmail.com>
2024-12-31 15:16:47 +0530

    Arrays input 2

---
+++ Arraysinput2.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class Arraysinput2{
+    public static void main(String[] args){
+        int[] arr = {2,3,4,22,33,55,66};
+        for(int num : arr){
+            System.out.print(num);
+        }
+    }
+}
