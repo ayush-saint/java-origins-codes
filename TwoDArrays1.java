68b30ae34af67b197ee7728eb96526fe6b95f28b
Ayush Kumar <ayush22kumar37@gmail.com>
2025-01-01 12:15:12 +0530

    example of 2D Arrays

---
+++ TwoDArrays1.java
@@ -0,0 +1,14 @@
+// example of 2d array in java
+
+import java.util.Arrays;
+public class TwoDArrays1{
+    public static void main(String[] args){
+        int[][] arr = {
+            {1,2,3},
+            {3,22,3},
+            {3,22,33,333}
+        };
+        System.out.println(Arrays.deepToString(arr));
+    }
+
+}
