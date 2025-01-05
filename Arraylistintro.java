6262d671f785983d679656ac61878ab445227e00
ayush-saint <ayush22kumar37@gamil.com>
2025-01-05 14:56:17 +0530

    example of arraylist

---
+++ Arraylistintro.java
@@ -0,0 +1,14 @@
+//example of arraylist (dynamic array of java)
+
+import java.util.*;
+public class Arraylistintro{
+    public static void main(String[] args){
+    ArrayList<Integer> list = new ArrayList<>();
+    list.add(34);
+    list.add(322);
+    list.add(3222);
+    list.add(3422);
+    list.add(3344);
+    System.out.println(list);
+    }
+}
