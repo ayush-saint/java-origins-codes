7985ee25c44fdd56b50a281f61d1bcd877314382
Ayush Kumar <ayush22kumar37@gmail.com>
2025-03-23 18:41:52 +0530

    leetcodearrayQ1

---
+++ leetcodearrays/LeetcodearrayQ1.java
@@ -0,0 +1,56 @@
+
+/* leetcode array Q1..
+Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+ 
+
+Example 1:
+
+Input: nums = [0,2,1,5,3,4]
+Output: [0,1,2,4,5,3]
+Explanation: The array ans is built as follows: 
+ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+    = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
+    = [0,1,2,4,5,3]
+*/
+import java.util.Scanner;
+
+public class leetcodearrayQ1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enter the number of element of array: ");
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+
+        System.out.println("enter the element of array : ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println("arrays element are :");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        scanner.close();
+
+        int[] answer = buildarray(arr);
+        System.out.println("the required array is :");
+        for (int num : answer) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static int[] buildarray(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = arr[arr[i]];
+        }
+        return ans;
+    }
+}
